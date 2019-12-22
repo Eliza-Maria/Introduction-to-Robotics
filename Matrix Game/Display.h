83919e3ed471e9fd9044404d64c657884c78e687
Eliza-Maria <47538164+Eliza-Maria@users.noreply.github.com>
2019-12-22 15:04:10 +0200

    Update Display.h

--- Matrix Game/Display.h
+++ Matrix Game/Display.h
@@ -10,143 +10,122 @@ const int D5 = 4;
 const int D6 = 3;
 const int D7 = 2;
 
-int selectState;
-int lastSelectState;
+int selectState = 0;
+int lastSelectState = 0;
+int menuState = 0;
+int lastMenuState = 0;
+bool swVal2;
+
+unsigned int lastDebounceBack = 0;
+const int debounceBackInterval = 1000;
 
 LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
 
 
-//void displayScoreAndLevel()
-//{
-//	lcd.setCursor(1, 0);
-//	lcd.print("Score:");
-//	lcd.setCursor(8, 0);
-//	lcd.print((String)currScore);
-//	lcd.setCursor(1, 1);
-//	lcd.print("Level:");
-//	lcd.setCursor(8, 1);
-//	lcd.print((String)Level);
-//}
+void setupDisplay() {
 
-//void setupDisplay() {
-//	// put your setup code here, to run once:
-//
-//	pinMode(rX, INPUT);
-//	pinMode(rY, INPUT);
-//	pinMode(SW, INPUT_PULLUP);
-//
-//
-//	lcd.begin(16, 2); //columns, nr of rows
-//	lcd.clear();
-//
-//	menuState = 0;
-//	selectState = 0;
-//}
-//
-//void displayMenu(bool &gameOver, bool &gameRunning) {
-//	// put your main code here, to run repeatedly:
-//
-//	//readJoystickMenu(selectState)
-//	//int swVal = readJoystickButton();
-//
-//	if (swVal == 1)
-//	{
-//		if (menuState == 0)
-//		{
-//			menuState = selectState + 1;
-//		}
-//
-//		if (menuState == 1) //game started
-//		{
-//			gameRunning = true;
-//			gameOver = true;
-//			return;
-//		}
-//	}
-//
-//	
-//
-//	if ((lastMenuState != menuState) || (lastSelectState != selectState))
-//	{
-//		lcd.clear();
-//		lastMenuState = menuState;
-//		lastSelectState = selectState;
-//	}
-//
-//	switch (menuState) {
-//	case 0:
-//	{
-//		lcd.setCursor(1, 0);
-//		lcd.print("Start");
-//		lcd.setCursor(7, 0);
-//		lcd.print("HighScore");
-//		lcd.setCursor(5, 1);
-//		lcd.print("Settings");
-//
-//		switch (selectState)
-//		{
-//		case 0:
-//		{
-//			lcd.setCursor(0, 0);
-//			lcd.print(">");
-//			break;
-//		}
-//		case 1:
-//		{
-//			lcd.setCursor(6, 0);
-//			lcd.print(">");
-//			break;
-//		}
-//		case 2:
-//		{
-//			lcd.setCursor(4, 1);
-//			lcd.print(">");
-//			break;
-//		}
-//		}
-//		break;
-//	}
-//	case 1:
-//	{
-//		lcd.setCursor(0, 0);
-//		lcd.print("High Score:" + (String)highScore);
-//		break;
-//	}
-//	case 2:
-//	{
-//		/*lcd.setCursor(0, 0);
-//		lcd.print("Starting Level:");
-//		lcd.setCursor(0, 1);
-//		lcd.print((String)startingLevelValue);
-//		break;*/
-//	}
-//	}
-//
-//	/*if (menuState == 2)
-//	{
-//		if (yVal > maxThreshold&&joystickMovedY == false)
-//		{
-//			startingLevelValue++;
-//			if (startingLevelValue > 5)
-//			{
-//				startingLevelValue = 0;
-//			}
-//			joystickMovedY = true;
-//		}
-//
-//		if (yVal < minThreshold&&joystickMovedY == false)
-//		{
-//			startingLevelValue--;
-//			if (startingLevelValue < 0)
-//			{
-//				startingLevelValue = 5;
-//			}
-//			joystickMovedY = true;
-//		}
-//		if (yVal <= maxThreshold && yVal >= minThreshold)
-//		{
-//			joystickMovedY = false;
-//		}
-//	}*/
-//
-//}
+	lcd.begin(16, 2); //columns, nr of rows
+	lcd.clear();
+
+	menuState = 0;
+	selectState = 0;
+}
+
+void displayTimeAndLevel()
+{
+	unsigned long timeRemaining = ((levelStartedAt + Levels[currLevel].timeToComplete) - millis()) / 1000;
+	lcd.setCursor(0, 0);
+	lcd.print("Time:");
+	lcd.setCursor(8, 0);
+	lcd.print((String)timeRemaining);
+
+	lcd.setCursor(0, 1);
+	lcd.print("Level:");
+	lcd.setCursor(8, 1);
+	lcd.print((String)currLevel); 
+}
+
+void displayScore()
+{
+	lcd.setCursor(0, 0);
+	lcd.print("Score:");
+	lcd.setCursor(8, 0);
+	lcd.print((String)currScore);
+}
+
+void displayMenu(bool &showMenu, bool &reset) 
+{
+	reset = false;
+	readJoystickMenu(selectState);
+
+	swVal2 = digitalRead(SW);
+	if (swVal2 == LOW)
+	{
+
+		if (millis() - lastTimePressed > pressInterval)
+		{
+			lastTimePressed = millis();
+			if (menuState == 0)
+			{
+				menuState = selectState + 1;
+				lastDebounceBack = millis();
+			}
+			else if (menuState == 2)
+			{		
+				menuState = 0;
+				selectState = 0;
+			}
+		}
+	}
+
+	if (lastMenuState != menuState || lastSelectState != selectState)
+	{
+		lcd.clear();
+		lastMenuState = menuState;
+		lastSelectState = selectState;
+	}
+
+	switch (menuState)
+	{
+		case 0:
+		{
+			lcd.setCursor(4, 0);
+			lcd.print("Start");
+			lcd.setCursor(4, 1);
+			lcd.print("HighScore");
+
+			switch (selectState)
+			{
+			case 0:
+			{
+				lcd.setCursor(3, 0);
+				lcd.print(">");
+				break;
+			}
+			case 1:
+			{
+				lcd.setCursor(3, 1);
+				lcd.print(">");
+				break;
+			}
+			}
+			break;
+		}
+		case 1:
+		{
+			showMenu = false;
+			reset = true;
+			menuState = 0;
+			lastMenuState = 0;
+			selectState = 0;
+			break;
+		}
+		case 2:
+		{
+			lcd.setCursor(0, 0);
+			lcd.print("High Score:" + (String)highScore);
+			break;
+		}
+	}
+
+}
