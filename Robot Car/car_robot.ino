eaf2ebe732f0d4141e6b0a50291de069c2a7d5bb
Eliza-Maria <47538164+Eliza-Maria@users.noreply.github.com>
2020-02-26 11:08:26 +0200

    Add files via upload

---
+++ Robot Car/car_robot.ino
@@ -0,0 +1,225 @@
+#include <AFMotor.h>
+#include <Servo.h>
+#include <NewPing.h>
+
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
+AF_DCMotor motor2(2, MOTOR12_64KHZ);
+
+Servo servo_motor;
+
+//sensor pins
+const int trigPin = A5;
+const int echoPin = A3;
+
+#define maximum_distance 400
+boolean goesForward = false;
+int distance = 100;
+
+const int carSpeed = 200;
+const int safetyDistance = 10;
+
+NewPing sonar(trigPin, echoPin, maximum_distance); //sensor function
+
+
+//const double v = 0.034; //speed of sound though cm/s
+
+void setup() {
+  // put your setup code here, to run once:
+  motor1.setSpeed(carSpeed);
+  motor2.setSpeed(carSpeed);
+
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600);
+
+  servo_motor.attach(9);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  motor1.run(FORWARD);  
+//  motor2.run(FORWARD);      
+//  delay(1000); 
+//  motor1.run(BACKWARD);
+//  motor2.run(BACKWARD);       
+//  delay(1000);  
+//  motor1.run(RELEASE);  
+//  motor2.run(RELEASE);      
+
+
+//myservo.write(++val);
+//delay(15);
+
+//emits ultrasonic
+//  digitalWrite(trigPin, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);
+//
+//  duration = pulseIn(echoPin, HIGH);
+//
+//  //distance = time * speed
+//  distance = duration/2 * v;
+//  Serial.println(distance);
+
+
+//  if(millis()>timer)
+//  {
+//    goesForward = false;
+//  }
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(50);
+
+  if (distance <= safetyDistance)
+  {
+    Serial.println("backward");
+    moveStop();
+    delay(300);
+    moveBackward();
+    delay(400);
+    moveStop();
+    delay(300);
+    distanceRight = lookRight();
+    delay(300);
+    distanceLeft = lookLeft();
+    delay(300);
+
+    if (distance >= distanceLeft)
+    {
+      turnRight();
+      moveStop();
+    }
+    else
+    {
+      turnLeft();
+      moveStop();
+    }
+  }
+  else
+  {
+    Serial.println("forward");
+    moveForward(); 
+  }
+    distance = readPing();
+}
+
+int lookRight()
+{  
+  servo_motor.write(10);
+  delay(500);
+  int distance = readPing();
+ delay(100);
+  servo_motor.write(90);
+  return distance;
+}
+
+int lookLeft()
+{
+  servo_motor.write(170);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(90);
+  return distance;
+  delay(100);
+}
+
+int readPing()
+{
+  delay(70);
+  
+  int cm = sonar.ping_cm();
+  
+  if (cm==0)
+  {
+    cm=250;
+  }
+  return cm;
+}
+
+void moveStop(){
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  
+  motor1.run(BRAKE);
+  motor2.run(BRAKE);
+}
+
+void moveForward()
+{
+
+  if(!goesForward)
+  {
+     
+    goesForward=true;
+
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);
+  
+    delay(500);   
+    
+     motor1.run(FORWARD);
+     motor2.run(FORWARD);
+  }
+}
+//
+void moveBackward()
+{
+
+  goesForward=false;
+
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  
+  delay(500);
+  
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  
+}
+
+void turnRight()
+{
+
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  
+  delay(500);
+  
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  
+  delay(500);
+  
+//  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  
+  delay(500);
+  
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+}
+
+void turnLeft(){
+
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  
+  delay(500);
+  
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  
+  delay(500);
+  
+  motor1.run(RELEASE);
+//  motor2.run(RELEASE);
+  
+  delay(500);
+  
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+}
